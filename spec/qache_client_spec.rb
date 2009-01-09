96e446289d6b812b30bb91d41a1630bc6664eef7
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2009-01-09 23:19:50 +0100

    completing specs

--- spec/qache_client_spec.rb
+++ spec/qache_client_spec.rb
@@ -2,5 +2,14 @@ require File.join(File.dirname(__FILE__), "spec_helper")
 require "qache"
 
 describe "qache client" do
-  
+  it "should start a client" do
+    puts "mmmmm"
+    # first we start a server
+    @server = Qache::Server.start
+    # then we start a server
+    @client = Qache::Client.new
+    @server.stop
+    # wait some seconds to me sure the process is stopped
+    10.downto(0) { |i| sleep(1); puts i }
+  end
 end
--- spec/qache_server_spec.rb
+++ spec/qache_server_spec.rb
@@ -8,12 +8,37 @@ module QacheServerSpecHelper
       :log_level => "DEBUG"
     }
   end
+  
+  def process_exists?(str)
+    !(IO.popen('ps aux').read =~ /#{str}/).nil?
+  end
 end
 
 describe "qache server" do
   include QacheServerSpecHelper
   
   it "should start a server with default values" do
-    Qache::Server.start(logger_options)
+    # check if memcacheq is stopped
+    process_exists?("memcacheq").should be_false
+    @server = Qache::Server.new(logger_options)
+    # we run memcacheq
+    @server.run
+    # check if memcacheq is started
+    process_exists?("memcacheq").should be_true
+    # we stop memcacheq
+    @server.stop
+    process_exists?("memcacheq").should be_false
+  end
+  
+  describe "options" do
+    it "should deamonize" do
+      @server = Qache::Server.new(logger_options.merge({:daemonize => true}))
+      @server.daemonize?.should be_true
+    end
+    
+    it "should deamonize" do
+      @server = Qache::Server.new(logger_options.merge({:daemonize => false}))
+      @server.daemonize?.should be_false
+    end
   end
 end
